6acdb901ece3216309fac59207b14e1beb44a9e2
Andrew Koldyrev <koldyrev.andrew@gmail.com>
2017-11-21 22:11:54 +0700

    1/03

---
+++ Visitor/LogEntryWithInnerClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorInnet
+{
+    public class LogEntryVisitorBase : ILogEntryVisitor
+    {
+        public virtual void Visit(ExceptionLogEntry exceptionLogEntry)
+        { }
+        public virtual void Visit(SimpleLogEntry simpleLogEntry)
+        { }
+    }
+
+    public class DataBaseExceptionLogEntrySaver : LogSaverBase
+    {
+        public void SaveLogEntry(LogEntry logEntry)
+        {
+            logEntry.Accept(new ExceptionLogEntryVisitor(this));
+        }
+        private class ExceptionLogEntryVisitor : LogEntryVisitorBase
+        {
+            private readonly DataBaseExceptionLogEntrySaver _parrent;
+            public ExceptionLogEntryVisitor(DataBaseExceptionLogEntrySaver parrent)
+            {
+                _parrent = parrent;
+            }
+            public override void Visit(ExceptionLogEntryVisitor expectionLogEntry)
+            {
+                _parrent.SaveException(exceptionLogEntry);
+            }
+        }
+    }
+
+public class LogSaverBase
+    {
+    }
+
+    public interface ILogEntryVisitor
+    {
+    }
+}
